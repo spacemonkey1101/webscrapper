e3738929d205013535db9693d7b2fc160ab555d3
spacemonkey1101 <ritambh54@gmail.com1>
2022-01-12 21:14:12 +0530

    get html data using axios

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const PORT = 6000
 const axios = require('axios');
 const cheerio = require('cheerio');
+const { response } = require('express');
 const express = require('express');
 
 const app = express()
+
+const url = 'https://www.theguardian.com/international'
+
+axios(url)
+    .then(response =>{
+        const html = response.data 
+        console.log(html);
+    })
 
 app.listen(PORT , () => console.log(`Server running on PORT ${PORT}`))
